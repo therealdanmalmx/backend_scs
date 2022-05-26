4ec87bee0d80798c7b94ca9ed4f383dc308cb573
therealdanmalmx <danmalmx@gmail.com>
2022-05-26 22:33:00 +0200

    Add slug to abouts

--- schemas/abouts.js
+++ schemas/abouts.js
@@ -1,26 +1,36 @@
-export default{
-    name:'abouts',
-    title:'Abouts',
-    type: 'document',
-    fields:[
-        {
-            name:'title',
-            title:'Title',
-            type:'string'
-        },
-        {
-            name:'description',
-            title:'Description',
-            type:'string'
-        },
-        {
-            name:'imgUrl',
-            title:'ImgUrl',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        },
-        
-    ]
-}
+export default {
+  name: "abouts",
+  title: "Abouts",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImgUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
