36d09159556315f082705be078d81347ad77b53a
therealdanmalmx <danmalmx@gmail.com>
2022-05-28 20:31:54 +0200

    Update schema names

--- schemas/works.js
+++ schemas/works.js
@@ -1,50 +1,59 @@
 export default {
-    name: 'works',
-    title: 'Works',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
+  name: "works",
+  title: "Works",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200,
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
       },
-    
-      {
-        name: 'description',
-        title: 'Description',
-        type: 'string',
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "projectLink",
+      title: "Project Link",
+      type: "string",
+    },
+    {
+      name: "codeLink",
+      title: "Code Link",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImageUrl",
+      type: "image",
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'projectLink',
-        title: 'Project Link',
-        type: 'string',
-      },
-      {
-        name: 'codeLink',
-        title: 'Code Link',
-        type: 'string',
-      },
-      {
-        name: 'imgUrl',
-        title: 'ImageUrl',
-        type: 'image',
-        options: {
-          hotspot: true,
+    },
+
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [
+        {
+          name: "tag",
+          title: "Tag",
+          type: "string",
         },
-      },
-   
-      {
-        name: 'tags',
-        title: 'Tags',
-       type:'array',
-       of: [
-         {
-           name:'tag',
-           title:'Tag',
-           type:'string'
-         }
-       ]
-      },
-     
-    ],
-  };
+      ],
+    },
+  ],
+};
